746543da27d55f0b4bc43d9c82c491c78c2f264c
Liam Wheen <liamwheen@googlemail.com>
2022-05-29 20:15:58 +0100

    Add case sensitive but thinks K could be in middle

--- wordle.py
+++ wordle.py
@@ -11,7 +11,7 @@ def load_words():
 
 def read_cols(in_file):
     with open(in_file) as f:
-        cols = [line.strip().upper() for line in f]
+        cols = [line.strip() for line in f]
         cols, known = cols[:-1],cols[-1]
         if not known: known = ALPHA
     return cols, known
@@ -19,17 +19,17 @@ def read_cols(in_file):
 def get_let(cols=None, known=None):
     if cols:
         word_dic = dict(zip(range(1,6),cols))
-        
+
     if cols==None:
         print('---ORANGE LETTERS---')
-        col1 = input('First col lets:\n').upper()
-        col2 = input('Second col lets:\n').upper()
-        col3 = input('Third col lets:\n').upper()
-        col4 = input('Fourth col lets:\n').upper()
-        col5 = input('Fifth col lets:\n').upper()
+        col1 = input('First col lets:\n')
+        col2 = input('Second col lets:\n')
+        col3 = input('Third col lets:\n')
+        col4 = input('Fourth col lets:\n')
+        col5 = input('Fifth col lets:\n')
 
         word_dic = dict(zip(range(1,6),(col1,col2,col3,col4,col5)))
-   
+
         print('---AVAILABLE LETTERS---')
         known = input('All possible lets:\n').upper()
         if not known: known = ALPHA
@@ -42,9 +42,16 @@ def main(f):
     else: cols,known = None, None
 
     word_dic, known = get_let(cols, known)
-    in_word = ''.join(set(''.join([str(item) for item in word_dic.values()])))
+    in_word = ''.join(set(''.join([str(item) for item in
+                                word_dic.values()]))).upper()
     known_set = set(known)
-    opts = [''.join(known_set - set(word_dic[i])) for i in range(1,6)]
+    opts = []
+    for i in range(1,6):
+        if word_dic[i].isupper():
+            opts.append(word_dic[i])
+        else:
+            opts.append(''.join(known_set - set(word_dic[i])).upper())
+
     opts = [''.join(opt) for opt in it.product(*opts)]
     all_words = load_words()
     valid_opts = [opt for opt in opts if opt in all_words]
@@ -54,3 +61,4 @@ def main(f):
 
 if __name__=='__main__':
     main(sys.argv[1:])
+
